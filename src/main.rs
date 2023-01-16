1ab17222532c12687e2d6ddef121778221a6f13b
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2023-01-16 17:35:08 +0530

    pop last element
    - `pop` pops the last element and returns an `Option<T>`, that `T` being
      what type the vector stores.
      - `None` is returned only when the vector is empty otherwise `Some`
        contains the popped element.
    - using `match` to handle both cases.
      - the compile makes sure that we handle both cases.
      - when there is an element
      - when the vector is empty

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     let mut v: Vec<i32> = vec![1, 2, 3];
     let len: i32 = v.len() as i32; // return usize
     println!("{len}"); // 3
+
+    let last_elem: Option<i32> = v.pop();
+    match last_elem {
+        Some(x) => println!("Last element is: {x}"),
+        None => println!("cannot pop")
+    }
 }
